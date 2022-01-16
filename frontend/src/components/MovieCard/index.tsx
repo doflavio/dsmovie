52e020844fb957b809889b18d8bc0dc8778a02f5
doflavio <doflavio@gmail.com>
2022-01-16 11:24:54 -0300

    Pagination

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -1,4 +1,4 @@
-import MovieScore from "components/MovieScore";
+import MovieScore from 'components/MovieScore';
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
@@ -14,14 +14,13 @@ function MovieCard( { movie } : Props) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
-                
-
-                <Link to={`/form/${movie.id}`}>
+                <MovieScore count={movie.count} score={movie.score} />
+                <Link to={ `/form/${movie.id}` }>
                     <div className="btn btn-primary dsmovie-btn">Avaliar</div>
                 </Link>
             </div>
         </div>
+
     );
 }
 
